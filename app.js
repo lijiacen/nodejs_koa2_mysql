424893d04daeb04baf81d6cfc275a0a1588eebf6
lijiacen <50915968+lijiacen@users.noreply.github.com>
2020-07-13 22:40:09 +0800

    koa2获取请求参数的四种方式

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const Koa = require("koa");
+const parser = require("koa-bodyparser");
+const InitManager = require("./core/init");
 
 const app = new Koa();
-const InitManager = require("./core/init");
+
+app.use(parser());
 InitManager.initCore(app);
 
 app.listen(3000);
--- app/api/demo.js
+++ app/api/demo.js
@@ -1,7 +1,11 @@
 const Router = require("koa-router");
 const router = new Router();
 
-router.get("/demo", (ctx) => {
+router.post("/demo/:id/laster", (ctx) => {
+  const path = ctx.params; //url中的param
+  const query = ctx.request.query; //?后面的参数
+  const headers = ctx.request.header; //requset请求头
+  const body = ctx.request.body; //requset body
   ctx.body = "demo";
 });
 
