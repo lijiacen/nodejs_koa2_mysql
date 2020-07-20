a74cd7e99b55c892b287a859af1f09a0767a9676
lijiacen <50915968+lijiacen@users.noreply.github.com>
2020-07-20 21:32:27 +0800

    全局异常处理中间件

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const Koa = require("koa");
 const parser = require("koa-bodyparser");
 const InitManager = require("./core/init");
+const catchException = require("./middlewares/exception");
 
 const app = new Koa();
 
+app.use(catchException);
 app.use(parser());
 InitManager.initCore(app);
 
--- app/api/test.js
+++ app/api/test.js
@@ -3,6 +3,7 @@ const router = new Router();
 
 router.get("/test", (ctx) => {
   ctx.body = "test";
+  throw new Error("test error");
 });
 
 module.exports = router;
